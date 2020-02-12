5d3c80f527809a78f6338849774b70ef69cbbc94
profdetech <emmanuel.mouronval@gmail.com>
2020-02-12 18:15:34 +0100

    first

---
+++ robot.ts
@@ -0,0 +1,15 @@
+//% weight=12 color=#00aced icon="\uf1b9"
+namespace profdetech_melodie{
+
+	
+	
+    //% blockId=casa_de_papel
+    //% block="Melodie bella ciao"
+	//% weight=99 blockGap=24
+    export function casa_de_papel(): string[] {
+		return ["b3:4", "e4:4", "f#4:4", "g4:4", "e4:4", "R:4", "b3:4", "e4:4", "f#4:4", "g4:4", "e4:4", "R:4", "b3:4", "e4:4", "f#4:4", "g4:4", "R:4", "f#4:4", "e4:4", "g4:4", "R:4", "f#4:4", "e4:4", "e5:4", "e5:4", "e5:4", "R:4", "d5:4", "e5:4", "f5:4", "f5:4", "R:4", "f5:4", "e5:4", "d5:4", "f5:4", "e5:4", "R:4", "b4:4", "a4:4", "g4:4", "f#4:4", "r:4", "b4:4", "r:4", "g4:4", "f#4:4", "e4:4"];
+    }
+	
+	
+		
+}
