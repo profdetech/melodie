8922363b0397f62cc343d38e806e8588ce7ae056
profdetech <emmanuel.mouronval@gmail.com>
2020-02-12 18:57:11 +0100

    Update robot.ts

--- robot.ts
+++ robot.ts
@@ -4,14 +4,14 @@ namespace profdetech_melodie{
 	
 	
     //% blockId=casa_de_papel
-    //% block="Melodie bella ciao"
+    //% block="Melodie bella ciao (régler bpm à environ 90)"
 	//% weight=99 blockGap=24
     export function casa_de_papel(): string[] {
-		return ["b3:4", "e4:4", "f#4:4", "g4:4", "e4:4", "R:4", "b3:4", "e4:4", "f#4:4", "g4:4", "e4:4", "R:4", "b3:4", "e4:4", "f#4:4", "g4:4", "R:4", "f#4:4", "e4:4", "g4:4", "R:4", "f#4:4", "e4:4", "e5:4", "e5:4", "e5:4", "R:4", "d5:4", "e5:4", "f5:4", "f5:4", "R:4", "f5:4", "e5:4", "d5:4", "f5:4", "e5:4", "R:4", "b4:4", "a4:4", "g4:4", "f#4:4", "r:4", "b4:4", "r:4", "g4:4", "f#4:4", "e4:4"];
+		return ["b3:2", "e4:2", "f#4:2", "g4:2", "e4:2", "R:2", "b3:2", "e4:2", "f#4:2", "g4:2", "e4:2", "R:2", "b3:2", "e4:2", "f#4:2", "g4:2", "R:2", "f#4:2", "e4:2", "g4:2", "R:2", "f#4:2", "e4:2", "e5:2", "e5:2", "e5:2", "R:2", "d5:2", "e5:2", "f5:2", "f5:2", "R:2", "f5:2", "e5:2", "d5:2", "f5:2", "e5:2", "R:2", "b4:2", "a4:2", "g4:2", "f#4:2", "r:2", "b4:2", "r:2", "g4:2", "f#4:2", "e4:2"];
     }
 	
 	 //% blockId=star_wars
-    //% block="Melodie star wars"
+    //% block="Melodie star wars (régler bpm à environ 80)"
 	//% weight=99 blockGap=24
     export function star_wars(): string[] {
 		return ["d4:1", "d4:1", "d4:1", "g4:4", "d#5:4", "c#5:1", "c5:1", "a4:1", "a#5:4", "d#5:2", "c#5:1", "c5:1", "a4:1", "a#5:4", "d#5:2", "c#5:1", "c5:1", "c#5:1", "a4:4"];
